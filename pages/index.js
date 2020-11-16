dce8037fc4d2956c829a649538bd470d343a4894
Thiago Silva <thiago25pro@gmail.com>
2020-11-16 19:04:06 -0300

    new introduction

--- pages/index.js
+++ pages/index.js
@@ -22,11 +22,7 @@ export default function Home({ allPostsData }) {
         <title>{siteTitle}</title>
       </Head>
       <section className={utilStyles.headingMd}>     
-        <p>[Your Self Introduction]</p>
-        <p>
-          (This is a sample website - you’ll be building a site like this on{' '}
-          <a href="https://nextjs.org/learn">our Next.js tutorial</a>.)
-        </p>
+        <p>What is up, guys? I'm web/mobile developer/student and this is my first contact with Next.js.</p>
       </section>
       <section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
         <h2 className={utilStyles.headingLg}>Blog</h2>
